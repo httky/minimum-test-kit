30a92bf9549168974c70b26a900a30b482ee715f
httky <takuyahata.tech@gmail.com>
2020-10-15 15:56:48 +0900

    Modify: dist配下をビルドで生成するよう修正

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,41 @@
 const path = require('path')
+const glob = require('glob')
+const HTMLPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+const scriptDir = './src'
+const htmlDir = './src/pages'
+const entries = {}
+
+glob.sync('**/*.js', {
+  // ignore: '**/_*.js' // 除外したいやつ
+  cwd: scriptDir
+}).map((key) => {
+  entries[key] = path.resolve(scriptDir, key)
+})
+
+const htmlEntries = glob.sync('**/*.html', {
+  cwd: htmlDir
+}).map((key) => {
+  // optionはここ
+  // https://github.com/jantimon/html-webpack-plugin#options
+  return new HTMLPlugin({
+    inject: false, // 全てのjsを注入しない
+    filename: key,
+    template: path.join(__dirname, `${htmlDir}/${key}`),
+  })
+})
+
+const plugins = htmlEntries
+const cleanPlugin = new CleanWebpackPlugin()
+const copyPlugin = new CopyPlugin({
+  patterns: [
+    { from: './src/static', to: path.resolve(__dirname, 'dist') },
+  ]
+})
+plugins.push(copyPlugin)
+plugins.unshift(cleanPlugin)
 
 module.exports = env => {
   const MODE = env
@@ -6,7 +43,7 @@ module.exports = env => {
 
   return {
     mode: MODE,
-    entry: './src/index.js',
+    entry: entries,
     module: {
       rules: [
         {
@@ -25,11 +62,13 @@ module.exports = env => {
       ]
     },
     output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js'
+      filename: '[name]',
+      path: path.resolve(__dirname, 'dist')
     },
     devServer: {
+      host: '0.0.0.0',
       contentBase: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: plugins
   }
 }
